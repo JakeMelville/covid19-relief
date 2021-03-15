46cc93aa703735fd3e390006e31f062096c25fbc
Jake Melville <jakemelville@ymail.com>
2021-03-15 11:50:37 -0400

    created mapQuestApi.js file. Connects with the mapquest api- when the user inputs a location, it will return the latitdude and longitude

---
+++ mapQuestApi.js
@@ -0,0 +1,15 @@
+const apiUrl = 'http://www.mapquestapi.com/geocoding/v1/address?key=';
+const apiKey =  'WFU3qGtFSrPpKOpC8vhpd3bfXKHEP4My';
+const locationInput = 'Washington, DC' //user input;
+
+const queryString = apiUrl + apiKey + '&location=' + locationInput;
+
+fetch(queryString)
+.then((res) => res.json())
+.then((data) => {
+    console.log(data.results[0].locations[0].latLng.lat)
+    console.log(data.results[0].locations[0].latLng.lng)
+    document.querySelector('body').textContent = data;
+})
+
+console.log(queryString);
