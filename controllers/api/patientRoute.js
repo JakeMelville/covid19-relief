ddfeaa04a98b20348be95fe35e13a8e827c8ec9b
dvaghani <dhruvi.vaghani@gmail.com>
2021-03-17 21:54:17 -0400

    saving any recent changes

--- controllers/api/patientRoute.js
+++ controllers/api/patientRoute.js
@@ -4,7 +4,7 @@ const { User } = require('../../models');
 router.post('/', async (req, res) => {
     try {
         const patientData = await User.create(req.body);
-        res.status(200).json(tripData);
+        res.status(200).json(patientData);
     } catch (err) {
         res.status(400).json(err);
     }
