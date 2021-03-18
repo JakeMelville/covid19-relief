bd11cc1768f4a6391f2624b741bb75009545a6c7
dvaghani <dhruvi.vaghani@gmail.com>
2021-03-17 20:25:17 -0400

    saving routes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./contollers');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const session = require('express-session');
@@ -21,16 +21,16 @@ app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 //set up sessions
-const sess = {
-  secret: 'covid info',
-  resave: false,
-  saveUninitialized: false
-};
+// const sess = {
+//   secret: 'covid info',
+//   resave: false,
+//   saveUninitialized: false
+// };
 
-app.use(session(sess));
+// app.use(session(sess));
 
 // turn on routes
 app.use(routes);
