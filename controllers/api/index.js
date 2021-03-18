476f4a4095d465b68452935bd9e267eb9b540274
dvaghani <dhruvi.vaghani@gmail.com>
2021-03-18 11:11:16 -0400

    updating multipe files

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const userRoutes = require('./userRoutes');
+const locationRoutes = require('./locationRoute');
 
 router.use('/users', userRoutes);
+router.use('/locations', locationRoutes);
 
 module.exports = router;
