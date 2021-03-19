0858d7da5cd0db8d315131d13b27bb6882c75681
dvaghani <dhruvi.vaghani@gmail.com>
2021-03-19 00:30:32 -0400

    saving changes to html

--- controllers/api/patientRoutes.js
+++ controllers/api/patientRoutes.js
@@ -67,4 +67,13 @@ router.post("/logout", (req, res) => {
   }
 });
 
+// router.get('/signup', (req, res) => {
+//   if (req.session.loggedIn) {
+//     res.redirect('/');
+//     return;
+//   }
+
+//   res.render('signup');
+// });
+
 module.exports = router;
